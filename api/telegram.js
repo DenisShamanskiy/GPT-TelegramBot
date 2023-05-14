84dbd819b2f68d900e635ea25b83cae1da7236b2
Denis Shamanskiy <d.g.shaman@yandex.ru>
2023-05-15 01:39:12 +0300

    Дополнительная настройка 1

---
+++ api/telegram.js
@@ -0,0 +1,13 @@
+import bot from "./main.js";
+
+async function handler(req, res) {
+  try {
+    await bot.handleUpdate(req.body);
+    res.status(200).end();
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error processing update");
+  }
+}
+
+export { handler };
--- api/telegram.mjs
+++
@@ -1,4 +0,0 @@
-import { start } from "telebot-vercel";
-import bot from "../src/main.js";
-
-export default start({ bot });
